9a9f601b729fd152831ec283aabd23b0b1ef0e00
Vande Matram <sachinkumar934122@gmail.com>
2026-04-08 20:59:50 +0530

    Remove unnecessary comments in CourseException

--- online learning management system/src/main/java/lms/CourseException.java
+++ online learning management system/src/main/java/lms/CourseException.java
@@ -1,9 +1,5 @@
 package lms;
 
-/**
- * Custom exception for course-related errors.
- * Used when course operations fail.
- */
 public class CourseException extends Exception {
     public CourseException(String message) {
         super(message);
